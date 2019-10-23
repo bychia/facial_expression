2072ca5c3700f14247a46a38d8d6ba3a571e9cd7
ChrisChia <chrischia82@gmail.com>
2019-10-23 23:03:11 +0800

    doing the face detection on jupyter notebook

--- app.py
+++ app.py
@@ -1,38 +1,30 @@
+import numpy as np
 import cv2
+import matplotlib.pyplot as plt
+#%matplotlib inline
+from flask import request, jsonify
 
-app = Flask(__name__)
-FETCH_TOP_N_FEATURES = 10
+#app = Flask(__name__)
+#FETCH_TOP_N_FEATURES = 10
 
 def detect_face(image):
-    in_file = model.stream.read()
-    bytes_in_file = BytesIO(in_file)
-    buffered_reader_in_file = BufferedReader(bytes_in_file)
-    result = {"Incompatible model": "True"}
-    try:
-        #out_dict = dill.load(buffered_reader_in_file)
-        out_dict = joblib.load(buffered_reader_in_file)
-        model = out_dict["model"]
-        model_type = out_dict["model_type"]
-        model_obj = model["model_object"]
-        feature_list = dict(zip(model["features"][:FETCH_TOP_N_FEATURES], (model_obj.feature_importances_.tolist())[:FETCH_TOP_N_FEATURES]))
-        result = {"_model_type": model_type,
-                 "threshold_to_hitrate": out_dict['threshold_to_hitrate'],
-                  "_top10_feature_list": sorted(feature_list.items(), key=lambda k: k[1], reverse=True)}
-    except:
-        pass
-    return result
+    test_image = cv2.imread(image)
+    # Converting to grayscale
+    test_image_gray = cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
+    # Displaying the grayscale image
+    plt.imshow(test_image_gray, cmap='gray')
 
 
-@app.route('/upload-photo', methods=['POST'])
+#@app.route('/upload-photo', methods=['POST'])
 def upload_photo():
-    if request.method == 'POST':
-        file = request.files['image']
-        out_dict = detect_face(file)
-        print(out_dict)
-        return jsonify(out_dict), 201
-    return jsonify({'error': 'Not found'}), 404
+    # if request.method == 'POST':
+    #     file = request.files['image']
+        detect_face("images/20191022_181144_994.jpg")
+    #     return jsonify(out_dict), 201
+    # return jsonify({'error': 'Not found'}), 404
 
+upload_photo()
 
-if __name__ == "__main__":
-    #app.run(host="0.0.0.0")
-    app.run(host="0.0.0.0", port=8081)
+# if __name__ == "__main__":
+#     #app.run(host="0.0.0.0")
+#     app.run(host="0.0.0.0", port=8081)
