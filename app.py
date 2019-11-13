3e61f2f6a7b9b8fdf08b961f2435c96bc3b7d63b
Chia, Chris <chris.chia@sap.com>
2019-11-13 18:43:15 +0800

    updated behaviour for backend

--- app.py
+++ app.py
@@ -3,17 +3,19 @@ from io import BufferedReader
 from flask import Flask, render_template, request, jsonify
 
 from keras.models import load_model
-from werkzeug import secure_filename
 from PIL import Image
-# from matplotlib import pyplot as plt
 import cv2
 import numpy as np
 import face_recognition
 import keras
 import copy
 import os
+import json
+import socket
+
 
 app = Flask(__name__)
+app.static_url_path = "/static"
 
 # haarcascade front face model
 haar_cascade_face = cv2.CascadeClassifier('data/haarcascades/haarcascade_frontalface_default.xml')
@@ -24,7 +26,7 @@ model = load_model("data/model_v6_23.hdf5")
 # expression dictionary
 emotion_dict= {'Angry': 0, 'Sad': 5, 'Neutral': 4, 'Disgust': 1, 'Surprise': 6, 'Fear': 2, 'Happy': 3}
 
-image_dir = os.path.join(app.root_path, 'images')
+static_dir = os.path.join(app.root_path, 'static')
 
 def converted_to_gray(image):
     # Converting to grayscale
@@ -57,9 +59,20 @@ def facial_expression():
 
     if request.method == 'POST':
         image = request.files['image']
-        image.save(os.path.join(image_dir, "img_captured.jpg"))
+        image.save(os.path.join(static_dir, "img_original.jpg"))
 
-        original_image = cv2.imread(os.path.join(image_dir, "img_captured.jpg"))
+        original_image = cv2.imread(os.path.join(static_dir, "img_original.jpg"))
+        cv2.imwrite(os.path.join(static_dir, 'img_captured.jpg'), original_image)
+
+        y = 15
+        x = 0
+        h = 55
+        w = 120
+        captured_image = cv2.imread(os.path.join(static_dir, "img_captured.jpg"))
+        captured_image = cv2.resize(captured_image, (120,88))
+        captured_image = captured_image[y:y+h, x:x+w]
+        cv2.imwrite(os.path.join(static_dir, 'img_captured.jpg'), captured_image)
+
         processed_image = copy.copy(original_image)
         processed_image = converted_to_gray(processed_image)
         faces_rects = haar_cascade_face.detectMultiScale(processed_image, scaleFactor=1.2, minNeighbors=5);
@@ -70,13 +83,22 @@ def facial_expression():
             face_image = processed_image[y:y + h, x:x + w]
             (width, height) = (48, 48)
             face_image = cv2.resize(face_image, (width, height))
-            cv2.imwrite(os.path.join(image_dir, 'resized_img_captured.jpg'), face_image)
-            face_image = cv2.imread(os.path.join(image_dir, 'resized_img_captured.jpg'))
+            cv2.imwrite(os.path.join(static_dir, 'resized_img_captured.jpg'), face_image)
+            face_image = cv2.imread(os.path.join(static_dir, 'resized_img_captured.jpg'))
             expression = predict_expression(array_from_image(face_image))
-            result = {"Expression": expression}
+            result = {"Expression": expression, "HostIP": get_host_ip(), "Image": 'resized_img_captured.jpg'}
+            with open(os.path.join(static_dir, 'result.json'), 'w') as f:
+                json.dump(result, f)
         return jsonify(result), 201
 
     return jsonify({'error': 'Not found'}), 404
+
+
+# Function to display hostname and
+# IP address
+def get_host_ip():
+    return request.host.split(':')[0]
+
 
 if __name__ == "__main__":
     #app.run(host="0.0.0.0")
